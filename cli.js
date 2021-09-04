159bb3f673869888df15170f2796a1da77ae26b7
Hyunsoo, Chung <admin@hyonsu.com>
2021-09-04 19:30:14 +0900

    Initial commit

---
+++ cli.js
@@ -0,0 +1,170 @@
+/**
+ * Angle calculation provided by PatrickKR
+ *
+ * Copyright (c) 2020 PatrickKR
+ *
+ * Do not remove this header.
+ */
+
+const fs = require('fs');
+const readline = require('readline');
+
+String.prototype.replaceAll = function (org, dest) {
+    return this.split(org).join(dest);
+}
+
+const utils = require('./utils');
+const { inputs } = require('./cli_input.json');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+process.stdout.write(inputs[0]);
+
+rl.on('line', line => {
+    input.push(line);
+    if(input.length < inputs.length) process.stdout.write(inputs[input.length]);
+    else rl.close();
+}).on('close', () => {
+    if(input.length < inputs.length) {
+        console.log('\n\n변환을 취소합니다.');
+        process.exit(0);
+    }
+
+    input = input.map(a => isNaN(a) ? a : Number(a));
+
+    if(!fs.existsSync(input[0])) {
+        console.log('해당 채보 파일이 없습니다.');
+        process.exit(0);
+    }
+
+    const file = fs.readFileSync(input[0]);
+    const adofai = utils.ADOFAIParser(file);
+
+    if(!adofai.pathData && !adofai.angleData) {
+        console.log('이 파일은 얼불춤 채보가 아닙니다.');
+        process.exit(0);
+    }
+
+    let pathData;
+    if(!adofai.pathData) pathData = utils.angleToPath(adofai.angleData);
+    else pathdata = adofai.pathData;
+
+    adofai.actions = adofai.actions.filter(a => a.eventType != 'SetHitsound');
+    adofai.settings.hitsound = 'Squareshot';
+
+    const roads = pathData.split('');
+
+    /**
+     * PatrickKR Start
+     */
+    let last_tile_angle = 180;
+    let twirl = false;
+
+    const angle_map = {
+        "L": 0,
+        "W": 15,
+        "H": 30,
+        "Q": 45,
+        "G": 60,
+        "q": 75,
+        "U": 90,
+        "o": 105,
+        "T": 120,
+        "E": 135,
+        "J": 150,
+        "p": 165,
+        "R": 180,
+        "A": 195,
+        "M": 210,
+        "C": 225,
+        "B": 240,
+        "Y": 255,
+        "D": 270,
+        "V": 285,
+        "F": 300,
+        "Z": 315,
+        "N": 330,
+        "x": 345,
+        "!": undefined,
+        "5": undefined,
+        "7": undefined
+    }
+    /**
+     * PatrickKR End
+     */
+
+    for (let i in roads) {
+        for (let key in angle_map) {
+            if (roads[i] == key) {
+                for (let sel in adofai.actions) {
+                    if (adofai.actions[sel].floor == i) {
+                        switch (adofai.actions[sel].eventType) {
+                            case 'Twirl':
+                                console.log(`twirl at ${i}`);
+                                twirl = !twirl;
+                                break;
+                        }
+                    }
+                }
+
+                /**
+                 * PatrickKR Start
+                 */
+                if (key == '!') {
+                    last_tile_angle -= 180;
+                    if (last_tile_angle < 0) {
+                        last_tile_angle += 360;
+                    }
+                    continue;
+                }
+
+                let result;
+                if (!isNaN(key)) result = 180 - 360 / (+key)
+                else result = 180 - (last_tile_angle - angle_map[key]);
+
+                if (twirl) result = 360 - result;
+
+                if (result <= 0) result += 360;
+                if (result > 360) result -= 360;
+
+                if (!isNaN(key)) last_tile_angle = twirl ? last_tile_angle + 360 / (+key) : last_tile_angle - 360 / (+key)
+                else last_tile_angle = angle_map[key];
+                /**
+                 * PatrickKR End
+                 */
+
+                if(result <= 30) {
+                    adofai.actions = adofai.actions.filter(a => a.eventType != 'SetHitsound' || a.floor < i || a.floor > i + 2);
+
+                    adofai.actions.push({
+                        "floor": Number(i),
+                        "eventType": "SetHitsound",
+                        "hitsound": "ReverbClack",
+                        "hitsoundVolume": 100
+                    });
+
+                    if(pathData.length >= Number(i) + 1) adofai.actions.push({
+                        "floor": Number(i) + 1,
+                        "eventType": "SetHitsound",
+                        "hitsound": "None",
+                        "hitsoundVolume": 100
+                    });
+
+                    if(pathData.length >= Number(i) + 2) adofai.actions.push({
+                        "floor": Number(i) + 2,
+                        "eventType": "SetHitsound",
+                        "hitsound": "Squareshot",
+                        "hitsoundVolume": 100
+                    });
+                }
+            }
+        }
+    }
+
+    fs.writeFileSync('export.adofai', JSON.stringify(adofai));
+});
---
+++ utils.js
@@ -0,0 +1,157 @@
+module.exports.ADOFAIParser = level => {
+    try {
+        return JSON.parse(level);
+    }
+    catch (e) {
+        return JSON.parse(String(level).trim()
+            .replaceAll(', ,', ',')
+            .replaceAll('}\n', '},\n')
+            .replaceAll('},\n\t]', '}\n\t]')
+            .replaceAll(', },', ' },')
+            .replaceAll(', }', ' }')
+            .replaceAll('\n', '')
+            .replaceAll('}\n', '},\n'));
+    }
+}
+
+const angleMap = {
+    0: "R",
+    15: "p",
+    30: "J",
+    45: "E",
+    60: "T",
+    75: "o",
+    90: "U",
+    105: "q",
+    120: "G",
+    135: "Q",
+    150: "H",
+    165: "W",
+    180: "L",
+    195: "x",
+    210: "N",
+    225: "Z",
+    240: "F",
+    255: "V",
+    270: "D",
+    285: "Y",
+    300: "B",
+    315: "C",
+    330: "M",
+    345: "A",
+    999: "!"
+}
+
+const flipPath = path => {
+    let r;
+
+    switch(path) {
+        case '5':
+            r = '6';
+            break;
+        case '6':
+            r = '5';
+            break;
+        case '7':
+            r = '8';
+            break;
+        case '8':
+            r = '7';
+            break;
+        case 'A':
+            r = 'x';
+            break;
+        case 'B':
+            r = 'F';
+            break;
+        case 'C':
+            r = 'Z';
+            break;
+        case 'D':
+            r = 'D';
+            break;
+        case 'E':
+            r = 'Q';
+            break;
+        case 'F':
+            r = 'B';
+            break;
+        case 'G':
+            r = 'T';
+            break;
+        case 'H':
+            r = 'J';
+            break;
+        case 'J':
+            r = 'H';
+            break;
+        case 'L':
+            r = 'R';
+            break;
+        case 'M':
+            r = 'N';
+            break;
+        case 'N':
+            r = 'M';
+            break;
+        case 'Q':
+            r = 'E';
+            break;
+        case 'R':
+            r = 'L';
+            break;
+        case 'T':
+            r = 'G';
+            break;
+        case 'U':
+            r = 'U';
+            break;
+        case 'V':
+            r = 'Y';
+            break;
+        case 'W':
+            r = 'p';
+            break;
+        case 'Y':
+            r = 'V';
+            break;
+        case 'Z':
+            r = 'C';
+            break;
+        case 'o':
+            r = 'q';
+            break;
+        case 'p':
+            r = 'W';
+            break;
+        case 'q':
+            r = 'o';
+            break;
+        case 'x':
+            r = 'A';
+            break;
+    }
+
+    return r;
+}
+
+module.exports.angleToPath = angleData => {
+    let pathData = '';
+    let lastPath = '';
+    angleData.forEach(a => {
+        let path;
+        if(a == 360) path = flipPath(lastPath);
+        else if(a < 0) path = angleMap[360 + a];
+        else path = angleMap[a];
+        lastPath = path;
+
+        if(!path) {
+            console.log('지원되지 않는 레벨입니다.');
+            process.exit(0);
+        }
+
+        pathData += path;
+    });
+
+    return pathData;
+}
